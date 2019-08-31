51488b315d77f50b9ee1ded163a37c7d066d1533
AlexToop <alextoop@outlook.com>
2019-08-31 13:39:33 +0100

    Init - basic task functionality

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # nodejs-alextoop-api
 API data source. 
+
+credit: https://www.codementor.io/olatundegaruba/nodejs-restful-apis-in-10-minutes-q0sgsfhbd
+
+install monogodb on platform of choice: https://docs.mongodb.com/manual/administration/install-community/
+MacOS run in foreground: 
+    mongod --config /usr/local/etc/mongod.conf
+
+npm init
+touch server.js
+mkdir api
+mkdir api/controllers api/models api/routes
+touch api/controllers/alextoopApiController.js api/models/alextoopApiModel.js api/routes/alextoopApiRoutes.js
+npm install --save-dev nodemon
+npm install express --save
+add "start": "nodemon server.js" to the package scripts.
+
---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3001,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/alextoopApiModel'), //created model loading here
+  bodyParser = require('body-parser');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/alextoopApi'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/alextoopApiRoutes'); //importing route
+routes(app); //register the route
+
+
+app.listen(port);
+
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found. Navigate to /content'})
+  });
+
+console.log('alextoop com\'s RESTful API server started on: ' + port);
