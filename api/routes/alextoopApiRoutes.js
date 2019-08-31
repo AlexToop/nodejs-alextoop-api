f4501aae282b846986ea57de1f19c3b4e6a121f5
AlexToop <alextoop@outlook.com>
2019-08-31 13:42:46 +0100

    Changed routes

--- api/routes/alextoopApiRoutes.js
+++ api/routes/alextoopApiRoutes.js
@@ -3,12 +3,12 @@ module.exports = function(app) {
   var todoList = require('../controllers/alextoopApiController');
 
 
-  app.route('/tasks')
+  app.route('/content')
     .get(todoList.list_all_tasks)
     .post(todoList.create_a_task);
 
 
-  app.route('/tasks/:taskId')
+  app.route('/content/:taskId')
     .get(todoList.read_a_task)
     .put(todoList.update_a_task)
     .delete(todoList.delete_a_task);
